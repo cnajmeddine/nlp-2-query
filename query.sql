c0cbb616963c2fc2a5423b65e369a76a78ae775b
Najm <109869412+cnajmeddine@users.noreply.github.com>
2025-01-13 13:58:28 +0000

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,1 +1,4 @@
-SELECT SUM(quantity * price) AS total_sales FROM sales;
+SELECT d.department_name, SUM(s.amount) AS total_sales
+FROM sales s
+JOIN departments d ON s.department_id = d.department_id
+GROUP BY d.department_name;
